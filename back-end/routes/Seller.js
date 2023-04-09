93823c8544b2fe6b1c4978a2df2351176fb34cc9
Zbiba <abdrrahmanabdo46@gmail.com>
2023-04-09 00:47:57 +0000

    created routes for updating getting removing products

--- back-end/routes/Seller.js
+++ back-end/routes/Seller.js
@@ -1,10 +1,32 @@
 const express = require('express')
+const multer = require('multer')
 const { authToken } = require('../controllers/AuthenticationController')
 const router = express.Router()
-const { addProduct ,updateProduct} = require('../controllers/SellerController')
+const {
+  addProduct,
+  updateProduct,
+  getProducts,
+  UploadProductImg,
+  removeProduct,
+} = require('../controllers/SellerController')
+
+router.get('/getProducts', authToken, getProducts)
 
 router.post('/addProduct', authToken, addProduct)
 
-router.post('/updateProduct' , authToken , updateProduct)
+router.post('/updateProduct', authToken, updateProduct)
 
+router.post('/removeProduct', authToken, removeProduct)
+
+const storageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, '../public/uploads')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  },
+})
+const upload = multer({ storage: storageEngine })
+
+router.post('/uploadProduct', upload.single('image'), UploadProductImg)
 module.exports = router
